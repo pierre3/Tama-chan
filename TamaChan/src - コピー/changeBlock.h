da8b2cd79aa75453b8ab9f22f288baab7a9c39fc
Hirotada Kobayashi <pierre3@outlook.com>
2024-12-11 09:24:10 +0900

    remove unnecessary files

--- TamaChan/src - コピー/changeBlock.h
+++
@@ -1,9 +0,0 @@
-/*chngeBlock.h*/
-#include "directD.h"
-
-
-//キャラクタのブロックに変換するタイミングをカウント
-extern void ChgBlkCount(_DDRAW,_CHAR*,_BGSCROLL*);
-
-//BGブロックをキャラクタのブロックに変換する
-void ChangeBlock(_CHAR*,int,int);
